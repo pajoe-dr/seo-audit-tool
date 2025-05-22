44febe4c9371adaca163f256448a80dff4ab5dbf
pajoe-dr <pajoedriz@gmail.com>
2025-05-22 00:43:34 -0500

    Update ontology_lookups.js

--- src/ontology_lookups.js
+++ src/ontology_lookups.js
@@ -2,31 +2,34 @@ const microdataParser = require('microdata-node');
 const Apify = require('apify');
 
 const jsonLdLookup = async (page) => {
-    // const url = page.url();
-    // console.time(`${url} jsonLdLookup`);
     let isJsonLd = false;
-    let jsonLdData = {};
-    if (await page.$('script[type="application/ld+json"]')) {
-        try {
-            isJsonLd = true;
-            jsonLdData = await page.$eval('script[type="application/ld+json"]', (el) => JSON.parse(el.innerText));
-        } catch (e) {
-            Apify.utils.log.warning(`Parsing LD+JSON failed: ${e.message}`);
+    let jsonLdData = [];
+
+    const handles = await page.$$eval('script[type="application/ld+json"]', scripts => {
+        return scripts.map(s => s.textContent.trim());
+    });
+
+    if (handles.length > 0) {
+        isJsonLd = true;
+        for (const rawJson of handles) {
+            try {
+                const parsed = JSON.parse(rawJson);
+                jsonLdData.push(parsed);
+            } catch (e) {
+                Apify.utils.log.warning(`Parsing LD+JSON failed: ${e.message}`);
+            }
         }
     }
-    // console.timeEnd(`${url} jsonLdLookup`);
+
     return { isJsonLd, jsonLdData };
 };
 
 const microdataLookup = async (page) => {
-    // const url = page.url();
-    // console.time(`${url} microdataLookup`);
     let isMicrodata = false;
     const pageHtml = await page.evaluate(() => document.documentElement.outerHTML);
     const microdata = microdataParser.toJsonld(pageHtml, {});
     if (microdata.length) isMicrodata = true;
 
-    // console.timeEnd(`${url} microdataLookup`);
     return { isMicrodata, microdata };
 };
 
